0fa01d2b8dd4f58d26930e03e26792450c7ae1cf
aaronshahriari <aaronshahriari@gmail.com>
2024-08-19 13:19:50 -0400

    update colors

--- config.h
+++ config.h
@@ -1,7 +1,7 @@
 /* See LICENSE file for copyright and license details. */
 
 /* appearance */
-static const unsigned int borderpx  = 2;        /* border pixel of windows */
+static const unsigned int borderpx  = 1;        /* border pixel of windows */
 static const unsigned int snap      = 32;       /* snap pixel */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
@@ -15,7 +15,7 @@ static const char outline[]         = "#4249a6";
 static const char main_col[]        = "#67A86C";
 static const char *colors[][3]      = {
 	/*               fg         bg         border   */
-	[SchemeNorm] = { light_grey, black, transparent },
+	[SchemeNorm] = { light_grey, black, black },
 	[SchemeSel]  = { black, main_col, outline },
 };
 
