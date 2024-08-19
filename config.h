7016ce94f1897cadc3983ba639906293809e07b5
aaronshahriari <aaronshahriari@gmail.com>
2024-08-19 16:36:58 -0400

    changed dmenu colors

--- config.h
+++ config.h
@@ -59,7 +59,7 @@ static const Layout layouts[] = {
 
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
-static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", black, "-nf", main_col, "-sb", black, "-sf", black, NULL };
+static const char *dmenucmd[] = { "dmenu_run", "-m", dmenumon, "-fn", dmenufont, "-nb", main_col, "-nf", black, "-sb", black, "-sf", white, NULL };
 static const char *termcmd[]  = { "alacritty", NULL };
 
 static const Key keys[] = {
