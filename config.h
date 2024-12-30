a1f019ef808b188875fe6f29965a169b94ae1201
aaronshahriari <aaronshahriari@gmail.com>
2024-12-30 05:19:43 -0500

    updated gimp to not be floating

--- config.h
+++ config.h
@@ -48,7 +48,7 @@ static const Rule rules[] = {
 	 *	WM_NAME(STRING) = title
 	 */
 	/* class      instance    title       tags mask     isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       1 << 4,            1,           -1 },
+	{ "Gimp",     NULL,       NULL,       1 << 4,            0,           -1 },
 	{ "Google-chrome",     NULL,       NULL,       1 << 1,            0,           -1 },
 };
 
